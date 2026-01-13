b59b2dd4a0ff6536dbe9359fd0c6e9342684d12b
Dana Sharon <whosedreamisthis@gmail.com>
2026-01-13 14:34:46 -0800

    spawn patients

---
+++ Assets/Scripts/PatientSpawner.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PatientSpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject patientPrefab;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() { }
+
+    void Spawn()
+    {
+        GameObject g = Instantiate(patientPrefab, transform.position, Quaternion.identity);
+        g.transform.parent = transform;
+
+        Invoke("Spawn", Random.Range(2, 8));
+    }
+}
